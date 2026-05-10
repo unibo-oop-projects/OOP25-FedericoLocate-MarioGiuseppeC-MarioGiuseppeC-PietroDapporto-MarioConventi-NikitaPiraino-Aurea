1189407fc1d26106156f7ffba1836fb712ce4a03
Nikita Piraino <nikita.piraino@studio.unibo.it>
2026-05-10 16:27:24 +0200

    created class ReportImpl.java

---
+++ src/main/java/it/unibo/aurea/view/ReportImpl.java
@@ -0,0 +1,17 @@
+package it.unibo.aurea.view;
+
+import java.util.Map;
+
+import it.unibo.aurea.model.api.ParameterType;
+import it.unibo.aurea.view.api.Report;
+
+public class ReportImpl implements Report{
+
+    @Override
+    public void show(String semesterLabel, Map<ParameterType, Integer> levels) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'show'");
+    }
+
+
+}
