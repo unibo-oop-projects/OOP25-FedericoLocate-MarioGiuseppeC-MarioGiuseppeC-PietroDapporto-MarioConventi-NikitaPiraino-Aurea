19fbb3a757f5bf4833f13d8e8f1969814dbfc049
Nikita Piraino <nikita.piraino@studio.unibo.it>
2026-05-20 23:16:15 +0200

    created the interface for the infoButton

---
+++ src/main/java/it/unibo/aurea/view/api/InfoButton.java
@@ -0,0 +1,14 @@
+package it.unibo.aurea.view.api;
+
+import javafx.scene.control.Button;
+
+public interface InfoButton {
+
+    /**
+     * Builds and returns the info button.
+     *
+     * @return the configured {@link Button}
+     */
+    Button build();
+
+}
