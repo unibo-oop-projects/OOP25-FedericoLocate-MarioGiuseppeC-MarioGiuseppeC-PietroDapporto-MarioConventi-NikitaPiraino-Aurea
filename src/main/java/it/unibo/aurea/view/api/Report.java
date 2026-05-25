0af3c9966e98ad831024a935fc15aef524981ef4
Nikita Piraino <nikita.piraino@studio.unibo.it>
2026-05-25 12:52:17 +0200

    corrected a comment

--- src/main/java/it/unibo/aurea/view/api/Report.java
+++ src/main/java/it/unibo/aurea/view/api/Report.java
@@ -43,7 +43,7 @@ public interface Report {
     void setButtonAction(HBox actions);
 
     /**
-     * @param background  is the new background, it's used when the background changes(endgame).
+     * @param background  is the new background, it's used when the background should change(ex. endgame).
      */
     void reveal(String background);
 
