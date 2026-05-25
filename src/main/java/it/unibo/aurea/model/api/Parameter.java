61947a570be960a8c09104353ab2da0b1c17ae3d
Nikita Piraino <nikita.piraino@studio.unibo.it>
2026-05-25 10:30:03 +0200

    now the interface Parameter is an extension of the ParameterView interface, it add only the modify method. I've also modified the comments

--- src/main/java/it/unibo/aurea/model/api/Parameter.java
+++ src/main/java/it/unibo/aurea/model/api/Parameter.java
@@ -1,51 +1,15 @@
 package it.unibo.aurea.model.api;
 
 /**
- * Represents the structure of the four parameter of the game.
- * 
+ * Mutable game parameter. Extends {@link ParameterView} with the ability to modify the level.
+ * Only the Model layer should hold references of this type.
  */
-public interface Parameter {
+public interface Parameter extends ParameterView {
     /**
-     * Indicates the level of the current parameter.
-     * 
-     * @return an {@code int} between 0/100
-     */
-    int getLevel();
-
-    /**
-     * Performs the raises or decreases of the level of this parameter. 
-     * Based on the level it can modify the life of this parameter.
-     * 
+     * Raises or decreases the level of this parameter.
+     *
      * @param delta the amount to add/subtract based on the sign.
      */
     void modify(int delta);
-
-    /**
-     * Indicates if the player can continue the game based on this parameter.
-     * 
-     * @return {@code true} if is > 0 && < 100, {@code false} otherwise
-     */
-    boolean isAlive();
-
-    /**
-     * Indicates the name of the current parameter.
-     * 
-     * @return the name of the parameter 
-     */
-    ParameterType getName();
-
-    /**
-     * Adds an observer that will be notified of any changes to this parameter.
-     *
-     * @param observer the {@code ParameterObserver} to add
-     */
-    void addObserver(ParameterObserver observer);
-
-    /**
-     * Retrieves the specific reason for the parameter's depletion or saturation.
-     *
-     * @return a String detailing the cause of game over related to this parameter.
-     */
-    String getDeathReason();
 }
 
